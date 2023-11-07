3987d42aa20e3d383d804271d71389de8f072ca8
Claudia Cortes <clau_cortes@unitec.edu>
2023-11-06 18:30:08 -0600

    Semana 5 - Renderizado condicional

--- Semana 4/clase31/src/components/lista.js
+++ Semana 4/clase31/src/components/lista.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Texto from './texto';
-import { useState } from "react";
 
 const Lista = () =>{
     const [texto, setTexto] = useState("Hola");
+    const [arreglo, setArreglo] = useState(["Manzana","Uva"]);
+    const multiple= ()=>{
+
+      if(texto){
+            if(texto==="adios"){
+                return <p>Que le vaya bien</p>
+            }else{
+                return <Texto/>
+            }
+        }else{
+            return <p>No hay nada que mostrar</p>
+        }
+    }
     return(
     <div>
         <h1>Ingrese el texto</h1>
         <input type="text" value={texto}  onChange={(event)=>{
             setTexto(event.target.value)
             }}/>
+            
             <p>{texto}</p>
-        <button>click me</button>
-        <Texto/>
+        <button >click me</button>
+            {/* {
+               texto&&<Texto/>
+            } */}
+
+            { texto ? <Texto/>  :<p>No hay nada que mostrar</p> }
+            {
+                multiple()
+            }
+            {
+                 arreglo.map(item =>{
+                    return <p>{item}</p>
+                })
+                
+            }
+        
     </div>
     )
 };
